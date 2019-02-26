261955adee5b51ef0599aa0649c4dd303d5a98a4
Farah Student <farah.gus@hotmail.com>
2019-02-26 12:04:07 -0500

    boilerplate and function stubs

---
+++ index.js
@@ -0,0 +1,72 @@
+'use strict';
+/* global $ */
+
+// FIRST PAGE
+// 1. user clicks on start button 
+
+
+// SECOND PAGE
+// 1. user selects option 
+// 2. user clicks next 
+// 3. sends answer 
+// updates store
+// refreshes page depending on chosen answer, doesn't go to next page yet 
+// updates progress bar and amount of questions right/wrong
+// shows correct answer
+// user clicks next
+
+const QUIZ = {
+    questions: [""],
+    allAnswers: [],
+    correctAnswer:[]
+};
+
+const STORE = {
+    questionPage: [currentQuestion, answerChoices, correctAnswer],
+    userResponse: [],
+    quizScore: [],
+};
+
+function generateAnswerList(){
+
+}
+
+function renderQuestionText(){
+
+}
+
+function startButton(){
+
+}
+
+function handleStartButton(){
+
+}
+
+function nextButton(){
+
+}
+
+function handleNextButton() {
+
+}
+
+function progressBar(){
+    // access the updated store and update progress bar
+}
+
+function quizScore(){
+
+}
+
+function restartButton(){
+
+}
+
+function handleRestartButton(){
+
+}
+
+
+
+$(handleAnswersSubmitted);
